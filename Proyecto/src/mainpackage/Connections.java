d7ebeb648280a92a5c23748b71e8f42d59ce99da
Sergio Moreno Antequera <seranmoreno500@gmail.com>
2018-11-27 09:58:32 +0100

    27/11/2018 SM
    Estructura

---
+++ Proyecto/src/mainpackage/Connections.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainpackage;
+
+/**
+ *
+ * @author windiurno
+ */
+public class Connections {
+    //Clase con todas las cosas publicas para no tener que estar
+    //Haciendo las connection 700 veces
+    
+    
+}
